10da55cb36ba46d1da9f66c448d8f834046dcefd
mahdesignstudio <mahdesignstudio@gmail.com>
2020-05-27 22:30:16 -0400

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -14,3 +14,25 @@ def say_hello_world_ten_times
 end
 
 say_hello_world_ten_times
+
+
+def greeting # Method Signature
+    puts "Hello World" # Method Body
+end # Method Closing 
+
+def greeting 
+    # Leave a line break for the method body
+    puts "Hello World" # Now code the body of the method
+end # Immediately close the method
+
+def greeting 
+    puts "Hello World"
+end 
+
+greeting # Executing the method by name 
+
+greeting # Executing the method again 
+
+
+
+
