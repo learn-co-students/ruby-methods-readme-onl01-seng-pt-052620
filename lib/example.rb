20e4d9ce1025d99d817a13461abe9f09b660b0ba
Sean B <butts.sean01@gmail.com>
2020-06-03 06:24:49 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,33 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+// ♥ irb
+2.6.1 :001 > phrase= "hello world!"
+ => "hello world!"
+2.6.1 :002 > 10.times do
+2.6.1 :003 >     puts phrase
+2.6.1 :004?>   end
+hello world!
+hello world!
+hello world!
+hello world!
+hello world!
+hello world!
+hello world!
+hello world!
+hello world!
+hello world!
+ => 10
+2.6.1 :005 > def greeting
+2.6.1 :006?>   puts "Hello World"
+2.6.1 :007?>   end
+ => :greeting
+2.6.1 :008 > greeting
+Hello World
+ => nil
+2.6.1 :009 > puts greeting
+Hello World
+
+ => nil
+2.6.1 :010 >
