5f733bd091d54839314335e914a96ea292590931
Justin Mathew <jmathew2111@yahoo.com>
2020-06-08 04:04:42 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,9 @@
-# Use this space to code along with the readme
-
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+    def greeting
+      puts "Hello World"
+    end
+     
+    greeting # Executing the method by name
+    #=> "Hello World"
+     
+    greeting # Executing the method again
+    #=> "Hello World"
